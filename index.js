c0521553d0a7256e20ef605ad88971c5c40a2853
emon-101 <emon26.ugv@gmail.com>
2026-06-17 08:38:10 +0600

    chore: add package.json with initial dependencies and configuration

---
+++ index.js
@@ -0,0 +1,54 @@
+const express = require('express')
+const cors = require('cors')
+const { MongoClient, ServerApiVersion } = require('mongodb');
+
+const app = express()
+const port = 5000
+require('dotenv').config();
+
+app.use(cors());
+app.use(express.json());
+
+app.get('/', (req, res) => {
+    res.send('Server is Runnig Fine')
+})
+
+
+const uri = process.env.MONGODB_URI
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    
+    const database = client.db("hire_loop");
+    const jobCollection = database.collection("jobs");
+
+    app.post('/api/jobs', async(req, res) => {
+        const job = req.body;
+        const result = await jobCollection.insertOne(job);
+        res.send(result);
+    })
+
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`)
+})
